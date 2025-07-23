3da492b1ceb218e64fec58fa411cd011006cf5cd
SaidiAziz <155757450+SaidiAziz@users.noreply.github.com>
2025-07-23 14:55:47 +0100

    Plan with TODO list for main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,27 @@
 from app.config import load_config
 
+
 def main():
     config = load_config()
     print("Document Processing System Started.")
     print(f"Loaded config: {config}")
 
+    print("Main application logic goes here.")
+
+    # TODO 1: take file from user
+
+    # TODO 2: check if file is pdf or image
+
+    # TODO 3: if pdf, extract text from pdf
+
+    # TODO 4: if image, extract text from image
+
+    # TODO 5: tokenize extracted text
+
+    # TODO 6: store tokens in database
+
+    # TODO 7: return tokens to user
+
+
 if __name__ == "__main__":
     main()
-
