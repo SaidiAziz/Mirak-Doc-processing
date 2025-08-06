1be2cac1b7dc7a53dbb63ddc74704a935c810bfd
SaidiAziz <155757450+SaidiAziz@users.noreply.github.com>
2025-08-06 13:01:46 +0100

    Implement TextExtractor class for PDF text extraction and add Tokenizer class for text tokenization and phonetic conversion; remove obsolete extraction and helper functions.

---
+++ app/ingestion/TextExtractor.py
@@ -0,0 +1,17 @@
+import pymupdf
+
+
+class TextExtractor:
+
+    
+    def extract_text_from_pdf(input_path):
+        """
+        Extract text from a PDF file using PyMuPDF.
+        """
+        doc = pymupdf.open(input_path)  # Open the PDF file
+        text = ""
+
+        for page_num in range(len(doc)):
+            page = doc.load_page(page_num)  # Load each page
+            text += page.get_text()  # Extract text from the page
+        return text
--- app/utils/helpers.py
+++
@@ -1,9 +0,0 @@
-import unicodedata
-
-
-def remove_accents(text):
-    """Normalize French diacritics to base characters"""
-    nfkd_form = unicodedata.normalize('NFKD', text)
-    return ''.join(c for c in nfkd_form if not unicodedata.combining(c))
-
-
